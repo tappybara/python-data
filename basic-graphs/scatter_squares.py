234902bb164e79bb86dfaf444bb3003c96309728
adothsieh <alexhsieh98@gmail.com>
2019-08-28 22:57:26 -0700

    Created Folders for organization. Begun work on dice classes

--- basic-graphs/scatter_squares.py
+++ basic-graphs/scatter_squares.py
@@ -5,7 +5,9 @@ y_values = [x**2 for x in x_values]
 
 pit.style.use('seaborn')
 fig, ax = pit.subplots()
-ax.scatter(x_values, y_values, s=100)
+
+# Colour map the y values based on value
+ax.scatter(x_values, y_values, c=y_values, cmap=pit.cm.Blues, s=100)
 
 # Set chart title and label axies.
 ax.set_title("Square Numbers", fontsize=24)
@@ -17,4 +19,8 @@ ax.tick_params(axis='both', which='major', labelsize=24)
 
 ax.axis([0, 1100, 0, 1100000])
 
+# Save figure as an image
+pit.savefig('squares_plot.png', bbox_inches='tight')
+
 pit.show()
+
