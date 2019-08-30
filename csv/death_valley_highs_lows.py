94d36b43bef4bbf59921ba249c5e64d803332422
adothsieh <alexhsieh98@gmail.com>
2019-08-29 22:25:53 -0700

    Death Valley Weather graph

--- csv/death_valley_highs_lows.py
+++ csv/death_valley_highs_lows.py
@@ -3,20 +3,24 @@ import matplotlib.pyplot as pit
 
 from datetime import datetime
 
-filename = 'data/sitka_weather_2018_simple.csv'
-
+filename = 'data/death_valley_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
+    # Get dates, and high and low temperatures from file with error checking
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = int(row[5])
-        low = int(row[6])
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 pit.style.use('seaborn')
 fig, ax = pit.subplots()
@@ -24,7 +28,9 @@ ax.plot(dates, highs, c='red', alpha=0.5)
 ax.plot(dates, lows, c='blue', alpha=0.5)
 pit.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
-pit.title("Daily high and low temperatures, 2018", fontsize=24)
+#Format plot
+title = "Dailiy high and low temperatures - 2018\nDeath Valley, CA"
+pit.title(title, fontsize=20)
 pit.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 pit.ylabel("Temperature (F)", fontsize=16)
